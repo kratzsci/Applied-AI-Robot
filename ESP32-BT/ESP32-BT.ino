7f244cebd7ea4af3b812d9a8bd062e752d7d72d2
Philipp Kratzsch <philipp.kratzsch@electrolux.com>
2022-11-26 21:45:19 -0500

    Wireless
    Wireless communication for esp32

--- ESP32-BT/ESP32-BT.ino
+++ ESP32-BT/ESP32-BT.ino
@@ -1,23 +1,12 @@
 #include "BluetoothSerial.h"
  
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
- 
 BluetoothSerial SerialBT;
- 
+
 void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
+  SerialBT.begin("ESP32");
 }
- 
+
 void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-  delay(20);
+  SerialBT.println("Hellow World");
+  delay(10000);
 }
