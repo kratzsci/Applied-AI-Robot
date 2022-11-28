260c23ae50d19ea9f91e460dfa05cd17685d5ca4
Philipp Kratzsch <philipp.kratzsch@electrolux.com>
2022-11-28 12:34:59 -0500

    Directions
    Updated directions script to take input as a function

--- WriteToWeb.py
+++ WriteToWeb.py
@@ -3,5 +3,35 @@ import webbrowser
 
 driver = webdriver.Chrome()
 driver.get("http://192.168.4.1/")
-button = driver.find_element("id","DRIVE")
-button.click()
+
+
+def Motors(direction):
+    if(direction == 0):
+        print("STOP")
+        button = driver.find_element("id", "STOP")
+    elif(direction == 1):
+        print("FORWARD")
+        button = driver.find_element("id", "DRIVE")
+    elif(direction == 2):
+        print("BACKWARD")
+        button = driver.find_element("id", "BACK")
+    elif(direction == 3):
+        print("OTHER")
+        button = driver.find_element("id", "OTHER")
+    elif(direction == 4):
+        print("RIGHT")
+        button = driver.find_element("id", "RIGHT")
+    elif(direction == 5):
+        print("LEFT")
+        button = driver.find_element("id", "LEFT")
+
+    button.click()
+
+
+while True:
+    direction = int(input())
+    Motors(direction)
+
+
+# button = driver.find_element("id","DRIVE")
+# button.click()
